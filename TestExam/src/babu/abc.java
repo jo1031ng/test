ae083e6530428e690335d0a9e68bfa040db39673
jo1031ng <75459168+jo1031ng@users.noreply.github.com>
2020-12-04 14:51:39 +0900

    test commit

---
+++ TestExam/src/babu/abc.java
@@ -0,0 +1,14 @@
+package babu;
+
+public class abc {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i=0,j=0;
+		for(;i>=10;i++)
+		{
+			System.out.println(i);
+		}
+	}
+
+}
